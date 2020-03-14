14c617fae44eb7b05ef2eb9a104cb9c730d8c22d
mameshev89@list.ru <mameshev89@list.ru>
2020-03-14 15:16:53 +0300

    Create model listcase

---
+++ Caselist/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Caselist.TodoModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Caselist
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<ToDoModel> _todoDate;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoDate = new BindingList<ToDoModel>()
+            {
+                new ToDoModel(){text="jhjhbj"},
+                new ToDoModel(){text="nbn"}
+            };
+            dgTodolist.ItemsSource=_todoDate;
+        }
+    }
+}
---
+++ Caselist/TodoModel/ToDoModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caselist.TodoModel
+{
+    public class ToDoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public bool is_done;
+        public string text;
+
+        public bool IsDone
+        {
+            get { return is_done; }
+            set { is_done = value; }
+        }
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+}
